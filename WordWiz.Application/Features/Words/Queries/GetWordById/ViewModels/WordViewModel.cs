adb451a0639a406c9f368e6f8e17ee0b387ac206
kkazdal <37306406+kkazdal@users.noreply.github.com>
2025-01-31 19:28:36 +0300

    fix: words tabşosundaki hata giderildi.

--- WordWiz.Application/Features/Words/Queries/GetWordById/ViewModels/WordViewModel.cs
+++ WordWiz.Application/Features/Words/Queries/GetWordById/ViewModels/WordViewModel.cs
@@ -2,8 +2,9 @@ namespace WordWiz.Application.Features.Words.Queries.GetWordById.ViewModels;
 
 public class WordViewModel
 {
-    public Guid Id { get; set; }
-    public string Text { get; set; } = null!;
-    public string? Description { get; set; }
-    public string? Example { get; set; }
-} 
+    public long Id { get; set; }
+    public string WordText { get; set; }
+    public string PartOfSpeech { get; set; }
+    public string Synonyms { get; set; }
+    public string Definition { get; set; }
+}
